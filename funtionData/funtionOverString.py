391a6811e8163236d0997f69402d7b7923b7245b
bingshenchen <50638538+bingshenchen@users.noreply.github.com>
2021-04-02 14:38:29 +0200

    Add files via upload

---
+++ funtionData/funtionOverString.py
@@ -0,0 +1,24 @@
+afbet =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','u','w','x','y','z']
+Afbet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'U', 'W', 'X', 'Y', 'Z']
+
+def is_kleine_letter(x):
+	if len(x) = 1 :
+		return x in afbet
+	return null
+
+def is_hoofd_letter(x):
+	if len(x) = 1 :
+		return x in Afbet
+	return null
+
+def vind_letter_in_afbet(x):
+	a = []
+	for n in x:
+		for k in afbet:
+			pass
+	return null
+
+def eerste_letter_to_grote(x):
+	if len(x) > 1 and x[0] in afbet:
+		pass
+		
