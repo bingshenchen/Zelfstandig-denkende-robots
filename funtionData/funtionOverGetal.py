b6fe2e01c82e629f6ad263263fdc538f2850d428
bingshenchen <50638538+bingshenchen@users.noreply.github.com>
2021-04-02 14:50:41 +0200

    Add files via upload

--- funtionData/funtionOverGetal.py
+++ funtionData/funtionOverGetal.py
@@ -1,12 +1,23 @@
 a = [for i in range(-999999,999999)]
+
+
 def is_Int(x):
 	return x in a
 
 def is_negatieve_getal(x):
 	return x<0
 
+#simple schrijven
+def ing(x):
+	is_negatieve_getal(x)
+
+
 def is_postive_getal(x):
 	return x>0
+
+def ipg(x):
+	is_postive_getal(x)
+
 
 def is_nul(x):
 	return x=0
@@ -20,5 +31,38 @@ def sum(int(a),int(b),int(c)):
 def sum(int(a),int(b),int(c),int(d)):
 	return a+b+c+d
 
-def min(int(a),int(b)):
+def sum(int(a),int(b),int(c),int(d),int(e), int(f)):
+	pass
+	
+def positieve_min(int(a),int(b)):
+	if a >= b:
+		return a-b
+	else if a < b:
+		return b-a
+	else return null
+
+def eerte_getaal_min_twede_getaal(int(a),int(b)):
 	return a-b
+
+def min_van_eerste_tot_laaste(int(a),int(b),int(c)):
+	return a-b-c
+	
+
+def min_van_eerste_tot_laaste(int(a),int(b),int(c), int(d)):
+	pass
+
+def maal(a,b):
+	return a*b
+
+def maal(a,b,c):
+	pass
+
+def maal(a,b,c,d):
+	pass
+
+def eerste_getal_deel_door_twede_getal(a, b):
+	pass
+
+def twede_getal_deel_door_eerste_getal(a, b):
+	pass
+
