bc832f75b72ddd3859cc530e99282300726d5a62
bingshenchen <50638538+bingshenchen@users.noreply.github.com>
2021-04-02 14:21:56 +0200

    Add files via upload

---
+++ funtionData/funtionOverGetal.py
@@ -0,0 +1,24 @@
+a = [for i in range(-999999,999999)]
+def is_Int(x):
+	return x in a
+
+def is_negatieve_getal(x):
+	return x<0
+
+def is_postive_getal(x):
+	return x>0
+
+def is_nul(x):
+	return x=0
+
+def sum(int(a),int(b)):
+	return a+b
+
+def sum(int(a),int(b),int(c)):
+	return a+b+c
+
+def sum(int(a),int(b),int(c),int(d)):
+	return a+b+c+d
+
+def min(int(a),int(b)):
+	return a-b
